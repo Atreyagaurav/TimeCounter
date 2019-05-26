69a27fd15249d18a875c347882bb0b79a7d2b41d
Gaurav Atreya <atreygaurv98@gmail.com>
2019-05-26 18:00:39 +0545

    Mae the window small, little translucent so it can be dragged off and put in a corner without much disturbance.

--- GRE_Counter/Form1.Designer.cs
+++ GRE_Counter/Form1.Designer.cs
@@ -29,8 +29,8 @@ namespace GRE_Counter
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.TextHH = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.TextMM = new System.Windows.Forms.Label();
             this.TextSS = new System.Windows.Forms.Label();
@@ -40,30 +40,19 @@ namespace GRE_Counter
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // TextHH
             // 
             this.TextHH.AutoSize = true;
-            this.TextHH.Font = new System.Drawing.Font("Microsoft Sans Serif", 99.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TextHH.Location = new System.Drawing.Point(286, 144);
+            this.TextHH.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextHH.Location = new System.Drawing.Point(65, 22);
             this.TextHH.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.TextHH.Name = "TextHH";
-            this.TextHH.Size = new System.Drawing.Size(259, 152);
+            this.TextHH.Size = new System.Drawing.Size(35, 20);
             this.TextHH.TabIndex = 3;
             this.TextHH.Text = "HH";
-            // 
-            // label1
-            // 
-            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(67, 9);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(1185, 108);
-            this.label1.TabIndex = 4;
-            this.label1.Text = "Time Remaining Till Exam ";
             // 
             // timer1
             // 
@@ -73,11 +62,11 @@ namespace GRE_Counter
             // TextMM
             // 
             this.TextMM.AutoSize = true;
-            this.TextMM.Font = new System.Drawing.Font("Microsoft Sans Serif", 99.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TextMM.Location = new System.Drawing.Point(604, 144);
+            this.TextMM.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextMM.Location = new System.Drawing.Point(125, 22);
             this.TextMM.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.TextMM.Name = "TextMM";
-            this.TextMM.Size = new System.Drawing.Size(289, 152);
+            this.TextMM.Size = new System.Drawing.Size(37, 20);
             this.TextMM.TabIndex = 5;
             this.TextMM.Text = "MM";
             this.TextMM.Click += new System.EventHandler(this.label2_Click);
@@ -85,66 +74,66 @@ namespace GRE_Counter
             // TextSS
             // 
             this.TextSS.AutoSize = true;
-            this.TextSS.Font = new System.Drawing.Font("Microsoft Sans Serif", 99.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TextSS.Location = new System.Drawing.Point(970, 144);
+            this.TextSS.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextSS.Location = new System.Drawing.Point(199, 22);
             this.TextSS.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.TextSS.Name = "TextSS";
-            this.TextSS.Size = new System.Drawing.Size(245, 152);
+            this.TextSS.Size = new System.Drawing.Size(33, 20);
             this.TextSS.TabIndex = 6;
             this.TextSS.Text = "SS";
             // 
             // TextDD
             // 
             this.TextDD.AutoSize = true;
-            this.TextDD.Font = new System.Drawing.Font("Microsoft Sans Serif", 99.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.TextDD.Location = new System.Drawing.Point(-4, 144);
+            this.TextDD.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextDD.Location = new System.Drawing.Point(1, 22);
             this.TextDD.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.TextDD.Name = "TextDD";
-            this.TextDD.Size = new System.Drawing.Size(259, 152);
+            this.TextDD.Size = new System.Drawing.Size(35, 20);
             this.TextDD.TabIndex = 7;
             this.TextDD.Text = "DD";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(211, 232);
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(34, 27);
             this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(104, 42);
+            this.label5.Size = new System.Drawing.Size(31, 13);
             this.label5.TabIndex = 8;
             this.label5.Text = "Days";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(517, 232);
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(95, 29);
             this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(118, 42);
+            this.label6.Size = new System.Drawing.Size(35, 13);
             this.label6.TabIndex = 9;
             this.label6.Text = "Hours";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(849, 232);
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(158, 29);
             this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(149, 42);
+            this.label7.Size = new System.Drawing.Size(44, 13);
             this.label7.TabIndex = 10;
             this.label7.Text = "Minutes";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(1169, 232);
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(231, 27);
             this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(165, 42);
+            this.label8.Size = new System.Drawing.Size(49, 13);
             this.label8.TabIndex = 11;
             this.label8.Text = "Seconds";
             // 
@@ -152,21 +141,35 @@ namespace GRE_Counter
             // 
             this.label9.Anchor = System.Windows.Forms.AnchorStyles.Top;
             this.label9.AutoSize = true;
-            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(142, 323);
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.Red;
+            this.label9.Location = new System.Drawing.Point(170, 6);
             this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(810, 108);
-            this.label9.TabIndex = 12;
+            this.label9.Size = new System.Drawing.Size(114, 16);
+            this.label9.TabIndex = 14;
             this.label9.Text = "Target Score: 338";
             this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(2, 6);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(168, 16);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Time Remaining Till Exam ";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1325, 661);
+            this.ClientSize = new System.Drawing.Size(295, 52);
             this.Controls.Add(this.label9);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
@@ -174,11 +177,16 @@ namespace GRE_Counter
             this.Controls.Add(this.TextDD);
             this.Controls.Add(this.TextSS);
             this.Controls.Add(this.TextMM);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.TextHH);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Opacity = 0.8D;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GRE 30th July 2019";
+            this.TopMost = true;
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -186,7 +194,6 @@ namespace GRE_Counter
 
         #endregion
         private System.Windows.Forms.Label TextHH;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label TextMM;
         private System.Windows.Forms.Label TextSS;
@@ -196,6 +203,7 @@ namespace GRE_Counter
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label1;
     }
 }
 
